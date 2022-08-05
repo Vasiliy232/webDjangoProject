edbbc77681d1891c1bf3fff35ce81dce6ac22217
Lisovskii Vasilii <lisovskii@biocad.ru>
2022-08-05 18:34:34 +0300

    Homework 8

--- homework_8/drumtraining/education/urls.py
+++ homework_8/drumtraining/education/urls.py
@@ -5,6 +5,9 @@ import education.views as education
 app_name = 'education'
 
 urlpatterns = [
+    path('course/create', education.CourseCreateView.as_view(), name='course_form'),
+    path('course/update/<pk>', education.CourseUpdateView.as_view(), name='course_update_form'),
+    path('course/delete/<pk>', education.CourseDeleteView.as_view(), name='course_confirm_delete'),
     path('', education.CourseListView.as_view(), name='courses_list'),
-    path('course/<pk>', education.CourseDetailView.as_view(), name='course_detail')
+    path('course/<pk>', education.CourseDetailView.as_view(), name='course_detail'),
 ]
--- homework_8/drumtraining/education/views.py
+++ homework_8/drumtraining/education/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.forms import ModelForm
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from education.models import Course
 
 
@@ -12,6 +19,18 @@ class PageTitleMixin:
         return context
 
 
+class CourseForm(ModelForm):
+    class Meta:
+        model = Course
+        fields = [
+            'name',
+            'started_at',
+            'duration',
+            'price',
+            'description'
+        ]
+
+
 class CourseListView(PageTitleMixin, ListView):
     model = Course
     page_title = 'Courses'
@@ -20,3 +39,26 @@ class CourseListView(PageTitleMixin, ListView):
 class CourseDetailView(PageTitleMixin, DetailView):
     model = Course
     page_title = 'Course'
+
+
+class CourseCreateView(PageTitleMixin, CreateView):
+    model = Course
+    form_class = CourseForm
+    page_title = 'Create course'
+
+    def get_success_url(self):
+        return reverse('education:course_detail', kwargs={'pk': self.object.pk})
+
+
+class CourseUpdateView(PageTitleMixin, UpdateView):
+    model = Course
+    form_class = CourseForm
+    page_title = 'Update course'
+
+    def get_success_url(self):
+        return reverse('education:course_detail', kwargs={'pk': self.object.pk})
+
+
+class CourseDeleteView(PageTitleMixin, DeleteView):
+    model = Course
+    success_url = reverse_lazy('education:courses_list')
